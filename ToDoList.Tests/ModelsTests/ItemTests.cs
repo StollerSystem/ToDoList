b44c55c35fcfd78d1b3829e25836045ac5342f58
Ben Stoller <stollersystemsound@gmail.com>
2020-09-22 22:28:15 -0700

    pass test to get items from list

--- ToDoList.Tests/ModelsTests/ItemTests.cs
+++ ToDoList.Tests/ModelsTests/ItemTests.cs
@@ -1,11 +1,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Models;
+using System.Collections.Generic;
+using System;
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
+
     [TestMethod]
     public void ItemConstructor_CreatesItems_Item()
     {
@@ -45,9 +52,24 @@ namespace ToDoList.Tests
       //Arrange
       List<Item> newList = new List<Item>{ };
       // Act
-      List<Item> result = Item.GetAll;
+      List<Item> result = Item.GetAll();
       //Assert
-      Assert.AreEqual(newList, result);
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsItems_ItemList()
+    {
+      // Arrange
+      string description01 = "Walk the dog";
+      string description02 = "Do the Dishers";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+      List<Item> newList = new List<Item> { newItem1, newItem2};
+      //Act
+      List<Item> result = Item.GetAll();
+      //Assert
+      CollectionAssert.AreEqual(newList,result);
     }
     
   }
--- ToDoList/Models/Items.cs
+++ ToDoList/Models/Items.cs
@@ -12,6 +12,11 @@ namespace ToDoList.Models
       return _instances;
     }
 
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
     public Item(string description)
     {
       Description = description;
